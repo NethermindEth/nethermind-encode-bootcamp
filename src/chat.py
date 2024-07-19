a9f1c4bbaa225776f40d6af26d9f9e32343ae722
Konrad Kokosa <kkokosa@users.noreply.github.com>
2024-07-19 18:42:24 +0200

    Improved user rating

--- src/chat.py
+++ src/chat.py
@@ -23,19 +23,6 @@ st.sidebar.title("Settings")
 model = st.sidebar.selectbox("Choose OpenAI model", ["gpt-3.5-turbo", "gpt-4", "gpt-4o"], index=2)
 temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.1)
 traits = st.sidebar.text_input("Enter traits", value="funny, grumpy")
-if rating := st.sidebar.radio(
-    "Rate the last answer",
-    options=[1, 2, 3, 4, 5],
-    format_func=lambda x: ":star:" * x,
-    horizontal=True
-):
-    if 'trace_id' in st.session_state:
-        print(f'trace_id={st.session_state.trace_id} rating={rating}')
-        langfuse.score(
-            name="feedback-on-trace",
-            value=rating,
-            trace_id=st.session_state.trace_id
-        )
 
 # Print message history
 for message in st.session_state.messages:
@@ -61,6 +48,7 @@ def generate_answer(user_prompt):
 @observe(as_type="generation")
 def stream_content_openai(user_prompt):
     st.session_state.trace_id = langfuse_context.get_current_trace_id()
+    st.session_state.observation_id = langfuse_context.get_current_observation_id()
     prompt = langfuse.get_prompt("personality-prompt", type="chat")
     system_prompt = prompt.compile(traits=traits)[0]
 
@@ -91,6 +79,7 @@ def stream_content_openai(user_prompt):
 
 
 # Handle prompt input
+response = None
 if input := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(input)    
@@ -100,3 +89,22 @@ if input := st.chat_input("What is up?"):
 
     st.session_state.messages.append({"role": "user", "content": input})
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+if "trace_id" in st.session_state:
+    with st.container():
+        col1, col2 = st.columns([3, 1])
+        with col2:
+            with st.popover("Rate answer"):
+                if rating := st.radio("Rate the last answer", 
+                            options=[1, 2, 3, 4, 5], 
+                            format_func=lambda x: ":star:" * x, 
+                            horizontal=True,
+                            index=None,
+                            key=st.session_state.trace_id):
+                    langfuse.score(
+                        id=st.session_state.trace_id,
+                        name="user-rating",
+                        value=rating,
+                        trace_id=st.session_state.trace_id,
+                        observation_id=st.session_state.observation_id
+                    )
