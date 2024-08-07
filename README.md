3b2b541f0d0b51985369c968bf3d490d71917358
Konrad Kokosa <kkokosa@users.noreply.github.com>
2024-08-07 11:47:20 +0200

    Brownbag version

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# nethermind-encode-bootcamp
+# Streamlit and Langfuse showcase
+
+## Running the application
+
+1. Run required Langfuse and Postgres:
+
+```bash
+docker-compose up
+```
+
+2. Go to `http://localhost:3000/` and create a new Project there
+3. In the Project settings create a new API keys (remember both public and privte key)
+4. Set up environment variables with the key from the previous point (Powershell example):
+
+```powershell
+$Env:LANGFUSE_SECRET_KEY="sk-lf-..."
+$Env:LANGFUSE_PUBLIC_KEY="pk-lf-..."
+$Env:LANGFUSE_HOST="http://localhost:3000"
+```
+
+5. Set up also `OPENAI_API_KEY` environment variable
+6. Run the main script:
+
+```bash
+streamlit run .\src\chat.py
+```
+
+7. You can also run the evaluation script:
+
+```bash
+python .\src\evaluate.py
+```
+
+*Note: This repository was created as a part of [Encode AI Bootcamp](https://www.encode.club/ai-bootcamp).*
--- src/evaluate.py
+++ src/evaluate.py
@@ -1,13 +1,16 @@
 import os
- 
+from rich.progress import Progress, SpinnerColumn, TextColumn, BarColumn, TaskProgressColumn, TimeElapsedColumn, TimeRemainingColumn, MofNCompleteColumn
 from langfuse import Langfuse
 from langchain.evaluation import load_evaluator
 from langchain_openai import OpenAI
 from langchain.evaluation.criteria import LabeledCriteriaEvalChain
+from datetime import datetime, timedelta
+from pydantic import ValidationError
 
 langfuse = Langfuse()
 langfuse.auth_check()
 
+# https://python.langchain.com/v0.1/docs/guides/productionization/evaluation/string/criteria_eval_chain/
 EVAL_TYPES={
     "conciseness": True,
     "relevance": True,
@@ -26,7 +29,11 @@ def fetch_all_pages(name=None, user_id = None, limit=50):
     all_data = []
  
     while True:
-        response = langfuse.get_generations(name=name, limit=limit, user_id=user_id, page=page)
+        response = langfuse.get_generations(name=name, 
+                                            limit=limit, 
+                                            user_id=user_id, 
+                                            page=page,
+                                            from_start_time=datetime.now() - timedelta(days=1))
         if not response.data:
             break
  
@@ -39,22 +46,42 @@ def get_evaluator_for_key(key: str):
   llm = OpenAI(temperature=0, model="gpt-3.5-turbo-instruct")
   return load_evaluator("criteria", criteria=key, llm=llm)
 
-def execute_eval_and_score(generations): 
-  import tqdm
-
-  total_generations = len(generations)
-  with tqdm.tqdm(total=total_generations, desc="Evaluating Generations") as pbar:
+def execute_eval_and_score(generations):
+  total_generations = len(generations) * len(EVAL_TYPES)
+  with Progress(
+      SpinnerColumn(),
+      TextColumn("[progress.description]{task.description}"),
+      BarColumn(bar_width=None),
+      TaskProgressColumn(),
+      TimeElapsedColumn(),
+      TimeRemainingColumn(),
+      MofNCompleteColumn(),
+      expand=True
+  ) as progress:
+    task = progress.add_task("Evaluating Generations", total=total_generations)
     for generation in generations:
       criteria = [key for key, value in EVAL_TYPES.items() if value and key != "hallucination"]
-   
       for criterion in criteria:
-        eval_result = get_evaluator_for_key(criterion).evaluate_strings(
+        try:
+          eval_result = get_evaluator_for_key(criterion).evaluate_strings(
             prediction=generation.output,
             input=generation.input,
-        )
-        langfuse.score(name=criterion, trace_id=generation.trace_id, observation_id=generation.id, value=eval_result["score"], comment=eval_result['reasoning'])
-      pbar.update(1)
+          )
+          progress.update(task, advance=1)
+          if eval_result["score"] is None:
+            print(f"Skipping score for criterion '{criterion}' in generation {generation.id}: Score is None")
+            continue
+          langfuse.score(name=criterion, trace_id=generation.trace_id, observation_id=generation.id, value=eval_result["score"], comment=eval_result['reasoning'])
+        except ValidationError as e:
+          print(f"Validation error for criterion '{criterion}' in generation {generation.id}: {str(e)}")
+        except Exception as e:
+          print(f"Error evaluating criterion '{criterion}' in generation {generation.id}: {str(e)}")
+      
  
-generations = fetch_all_pages(user_id='test_user')
-execute_eval_and_score(generations)
-langfuse.flush()
+try:
+  generations = fetch_all_pages(user_id='test_user')
+  execute_eval_and_score(generations)
+  langfuse.flush()
+except Exception as e:
+  print(f"Error fetching generations: {str(e)}")
+
